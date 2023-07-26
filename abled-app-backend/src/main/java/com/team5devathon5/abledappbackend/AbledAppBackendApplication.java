f46dcda9271c6e9652313287f970a412ac9d6094
lufegaba75 <57178698+lufegaba75@users.noreply.github.com>
2023-07-26 18:52:08 +0200

    Add files via upload

---
+++ abled-app-backend/src/main/java/com/team5devathon5/abledappbackend/AbledAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.team5devathon5.abledappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbledAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbledAppBackendApplication.class, args);
+	}
+
+}
