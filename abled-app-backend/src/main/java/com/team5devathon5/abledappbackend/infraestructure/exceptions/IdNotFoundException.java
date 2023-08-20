64d516bf706291dce6db89bafe0abd2add9c0657
lufegaba75 <57178698+lufegaba75@users.noreply.github.com>
2023-08-20 11:12:32 +0200

    feat: added Exception Handler

---
+++ abled-app-backend/src/main/java/com/team5devathon5/abledappbackend/infraestructure/exceptions/IdNotFoundException.java
@@ -0,0 +1,6 @@
+package com.team5devathon5.abledappbackend.infraestructure.exceptions;
+
+public class IdNotFoundException extends RuntimeException{
+
+
+}
---
+++ abled-app-backend/src/main/java/com/team5devathon5/abledappbackend/utilities/Tables.java
@@ -0,0 +1,6 @@
+package com.team5devathon5.abledappbackend.utilities;
+
+public enum Tables {
+    users,
+    lodging
+}
